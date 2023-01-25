6649c0d3dee32219935ccce239c9fea8c0ca62c7
fr0sty-m <mertcan.053@hotmail.com>
2023-01-26 02:30:16 +0300

    Update settings.hpp

--- include/settings.hpp
+++ include/settings.hpp
@@ -1,14 +1,17 @@
 #ifndef SETTINGS_HPP
 #define SETTINGS_HPP
 
+// Window - Game settings
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 #define SCREEN_TITLE "roach engine"
 #define FPS 120
 
+// Color definitions
 #define PLAYER_COLOR 255,0,0,255
 #define GAME_BG_COLOR 62, 115, 179, 255
 
+// When you're adding path put ../ in front of assets otherwise it won't load file
 #define HELLO_IMG_PATH "../assets/hello.png"
 
 #endif // !SETTINGS_HPP
